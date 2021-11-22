2e128f1ebe688721505fc984251e164e193df109
Guilherme Abel <gpeuvis@gmail.com>
2021-11-21 22:45:23 -0300

    Update Transaction.ts

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,13 +1,9 @@
 import { v4 as uuid } from 'uuid';
 
 class Transaction {
-  
   id: string;
-  
   title: string;
-  
   value: number;
-  
   type: 'income' | 'outcome';
 
   constructor({ title, value, type }: Omit<Transaction, 'id'>) {
